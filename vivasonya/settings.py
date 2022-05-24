eeff90e0af65c69fccf0ea93de8017a3bf3807c5
qvog <qvog1@mail.ru>
2022-05-24 10:26:25 +0300

    fix

--- vivasonya/settings.py
+++ vivasonya/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'cart.context_processor.cart',
+                'cart.context_processors.cart',
             ],
         },
     },
--- vivasonya/urls.py
+++ vivasonya/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 
 from core.views import frontpage, shop
 from product.views import product
+from cart.views import add_to_cart
 
 urlpatterns = [
     path('', include('core.urls')),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
+    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_card'),
     path('admin/', admin.site.urls),
 ]
