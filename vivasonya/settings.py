00ced0506dad262801729c6e6620748b540e4bd9
qvog <qvog1@mail.ru>
2022-06-04 13:52:29 +0300

    static fix

--- vivasonya/settings.py
+++ vivasonya/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
