b83c9b1429037bd2118bb65f9fc0f3303ae07d8c
qvog <qvog1@mail.ru>
2022-06-06 17:40:41 +0300

    migrations

--- vivasonya/settings.py
+++ vivasonya/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8y@)8b7d*u=a6*j81j8z_pohuu_)h1m!m&hht)v%hebrgt&^sh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['nika-shop.herokuapp.com', '127.0.0.1']
 
@@ -93,8 +93,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_db',
-        'USER' : 'admin',
-        'PASSWORD' : 'admin',
+        'USER' : 'postgres',
+        'PASSWORD' : 'dancer2002',
         'HOST' : '127.0.0.1',
         'PORT' : '5432',
     }
