26d1f8a48f5d3e55374760b32c08e3fc1466e8cb
qvog <qvog1@mail.ru>
2022-06-18 18:41:30 +0300

    check

--- vivasonya/settings.py
+++ vivasonya/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8y@)8b7d*u=a6*j81j8z_pohuu_)h1m!m&hht)v%hebrgt&^sh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['nika-shop.herokuapp.com', '127.0.0.1']
 
