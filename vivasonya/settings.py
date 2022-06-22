fade17da4aaaa24b5343f79d70dfcd308b3fcc8c
Danila Afonkin <85894826+qvog@users.noreply.github.com>
2022-06-22 11:16:58 +0300

    Update settings.py

--- vivasonya/settings.py
+++ vivasonya/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_db',
         'USER' : 'postgres',
-        'PASSWORD' : 'dancer2002',
+        'PASSWORD' : '',
         'HOST' : '127.0.0.1',
         'PORT' : '5432',
     }
