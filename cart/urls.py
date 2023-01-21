9fd6ae3dc38b056b4f4d52f0400f12b2ba1ecd5a
qvog <qvog1@mail.ru>
2023-01-21 15:44:01 +0300

    success payment page

--- cart/urls.py
+++ cart/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from cart.views import add_to_cart, cart, checkout, hx_menu_cart, hx_cart_total, update_cart
+from cart.views import add_to_cart, cart, checkout, hx_menu_cart, hx_cart_total, update_cart, success
 
 urlpatterns = [
     path('', cart, name='cart'),
     path('checkout/', checkout, name='checkout'),
+    path('success/', success, name='success'),
     path('hx_menu_cart/', hx_menu_cart, name='hx_menu_cart'),
     path('hx_cart_total', hx_cart_total, name='hx_cart_total'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
